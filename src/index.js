3874b6f90cc6d726cd53081501916ca947c98fc9
jafaryNm <jaffarymdegela@gmail.com>
2025-03-07 10:21:14 +0300

    react-installation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+// 1. Import React and React library
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import React from "react";
+import ReactDom from 'react-dom/client'
+
+// 2 . Get reference with id root
+
+const el = document.getElementById('root');
+
+// 3. Tell react to get controll of that elements
+
+const root = ReactDom.createRoot(el)
+
+// 4. Create Components 
+
+function App(){
+  return <div>
+    <h1>Hi there react js</h1>
+  </div>
+}
+
+// 5 . Show components into the screen 
+
+root.render(<App/>)
